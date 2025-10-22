abcb9219f06e2394b98e3c753f83aa23f6a44cfc
Kalle Chen <kallechen.tw@gmail.com>
2025-10-22 12:29:36 +0800

    Update

--- dot_config/nvim/lua/config/nvim-tree.lua
+++ dot_config/nvim/lua/config/nvim-tree.lua
@@ -2,6 +2,7 @@ local nvim_tree = require("nvim-tree")
 
 nvim_tree.setup({
   sort_by = "case_sensitive",
+  sync_root_with_cwd = true, -- Keep nvim cwd aligned with tree root
   view = {
     width = 30,
   },
@@ -10,5 +11,9 @@ nvim_tree.setup({
   },
   filters = {
     dotfiles = false,  -- Show dotfiles
+  },
+  update_focused_file = {
+    enable = true, -- Reveal current buffer in the tree
+    update_root = true, -- Shift tree root to match focused file
   },
 })
